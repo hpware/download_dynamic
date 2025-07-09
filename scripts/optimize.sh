300a08fff50ea44bd6d92ee58367b900a5017ee0
吳元皓 <hw@yuanhau.com>
2025-07-09 22:04:07 +0800

    It should work now.

--- scripts/optimize.sh
+++ scripts/optimize.sh
@@ -1,7 +1,13 @@
-find . -type f -name "*.js" -name "*.css" -not -path "./node_modules/*" -print0 | while IFS= read -r -d $'\0' file; do
+# FIND JS
+find . -type f -name "*.js" -not -path "./node_modules/*" -print0 | while IFS= read -r -d $'\0' file; do
   echo "Uglifying (overwriting): $file"
   bunx uglifyjs "$file" \
     --compress --v8 --webkit \
     --comments "Copyright MIT LICENSE $(bun run scripts:getcurrentyear) Yuan-Hau Wu (yuanhau.com)" \
     -o "$file"
+done
+# FIND CSS
+find . -type f -name "*.css" -not -path "./node_modules/*" -print0 | while IFS= read -r -d $'\0' file; do
+  echo "Minifying CSS (overwriting): $file"
+  bunx cleancss -o "$file" "$file"
 done
