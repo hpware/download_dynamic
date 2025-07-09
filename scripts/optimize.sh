74fdaa1cf7aafdb207d454e95d2dd13a89269b20
吳元皓 <hw@yuanhau.com>
2025-07-09 22:06:17 +0800

    Add optimize scripts.

--- scripts/optimize.sh
+++ scripts/optimize.sh
@@ -1,6 +1,5 @@
 # FIND JS
 find . -type f -name "*.js" -not -path "./node_modules/*" -print0 | while IFS= read -r -d $'\0' file; do
-  echo "Uglifying (overwriting): $file"
   bunx uglifyjs "$file" \
     --compress --v8 --webkit \
     --comments "Copyright MIT LICENSE $(bun run scripts:getcurrentyear) Yuan-Hau Wu (yuanhau.com)" \
@@ -8,6 +7,5 @@ find . -type f -name "*.js" -not -path "./node_modules/*" -print0 | while IFS= r
 done
 # FIND CSS
 find . -type f -name "*.css" -not -path "./node_modules/*" -print0 | while IFS= read -r -d $'\0' file; do
-  echo "Minifying CSS (overwriting): $file"
-  bunx cleancss -o "$file" "$file"
+  minify --css "$file" > "$file"
 done
