968e027c3f9a4cfc75e76187dcefda1c38c2960f
吳元皓 <hw@yuanhau.com>
2025-07-09 22:11:22 +0800

    Remove the optimize css part.

--- scripts/optimize.sh
+++ scripts/optimize.sh
@@ -1,11 +1,6 @@
-# FIND JS
 find . -type f -name "*.js" -not -path "./node_modules/*" -print0 | while IFS= read -r -d $'\0' file; do
   bunx uglifyjs "$file" \
     --compress --v8 --webkit \
     --comments "Copyright MIT LICENSE $(bun run scripts:getcurrentyear) Yuan-Hau Wu (yuanhau.com)" \
     -o "$file"
-done
-# FIND CSS
-find . -type f -name "*.css" -not -path "./node_modules/*" -print0 | while IFS= read -r -d $'\0' file; do
-  minify --css "$file" > "$file"
 done
