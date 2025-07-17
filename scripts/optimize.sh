ab1ee44a2bff6208352a75e0a08338f433c3e360
吳元皓 <hw@yuanhau.com>
2025-07-17 20:34:58 +0800

    a

--- scripts/optimize.sh
+++ scripts/optimize.sh
@@ -1,6 +1,4 @@
 find . -type f -name "*.js" -not -path "./node_modules/*" -print0 | while IFS= read -r -d $'\0' file; do
   bunx uglifyjs "$file" \
-    --compress --v8 --webkit \
-    --comments "Copyright MIT LICENSE $(bun run scripts:getcurrentyear) Yuan-Hau Wu (yuanhau.com)" \
-    -o "$file"
+    --compress --v8 --webkit --comments "Copyright MIT LICENSE $(bun run scripts:getcurrentyear) Yuan-Hau Wu (yuanhau.com)" -o "$file"
 done
