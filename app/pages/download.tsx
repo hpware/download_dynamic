573ceba637cc22bf9ea2f85b4c4e1c7ee2405f8c
吳元皓 <hw@yuanhau.com>
2025-07-18 23:44:41 +0800

    BRUH

--- app/pages/download.tsx
+++ app/pages/download.tsx
@@ -80,7 +80,7 @@ function Page({ fileSQL }: { fileSQL: any }) {
 export default async function Export({ pathname }: { pathname: string }) {
   const findFile = await sql`
     SELECT * FROM file
-    WHERE path = ${pathname}
+    WHERE file_name = ${pathname}
     `;
   if (findFile.length === 0) {
     return (
