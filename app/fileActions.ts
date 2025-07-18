9e08c373abd5a3e9819cc574b26ea78fadc4c5ed
吳元皓 <hw@yuanhau.com>
2025-07-18 23:33:25 +0800

    Fix stuff.

--- app/fileActions.ts
+++ app/fileActions.ts
@@ -12,7 +12,7 @@ export async function AddFile(path: string, hash: string) {
         ${uuidv4()},
         ${path},
         ${uuidv4()},
-        ${fileNameWithoutExt},
+        ${fileNameWithoutExt.toLowerCase()},
         ${hash}
       )
       ON CONFLICT (path)
